6994b95d8f2ef69f8d5fccc66e7ca6d7e4a34337
AbdelRhman Samy <68971771+vemonser@users.noreply.github.com>
2022-07-26 07:03:34 +0200

    Add files via upload

---
+++ 24Hours.js
@@ -0,0 +1,68 @@
+import * as React from "react";
+
+const Svg24Hours = (props) => (
+  <svg
+    id="24-hours_svg__Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    x={0}
+    y={0}
+    viewBox="0 0 54 54"
+    style={{
+      enableBackground: "new 0 0 54 54",
+    }}
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style>{".24-hours_svg__st0{fill:#9e9fb4}"}</style>
+    <g id="24-hours_svg__Coworking-Space" transform="translate(-922 -977)">
+      <g id="24-hours_svg__About" transform="translate(378 796)">
+        <g id="24-hours_svg__features" transform="translate(0 180)">
+          <g transform="translate(544 1)" id="24-hours_svg__feature">
+            <path
+              id="24-hours_svg__XMLID_2047_"
+              className="24-hours_svg__st0"
+              d="M47 49.1H27C14.3 49.1 4 39 4 26.5S14.3 3.9 27 3.9c5 0 9.8 1.6 13.8 4.5h-1.1c-.5 0-1 .4-1 1s.4 1 1 1h3.7c.5 0 1-.4 1-1V5.7c0-.5-.4-1-1-1-.5 0-1 .4-1 1v1.5C38 3.8 32.6 2 27 2 20.3 2 14 4.5 9.3 9.2 4.6 13.8 2 20 2 26.5s2.6 12.7 7.3 17.3C14 48.5 20.3 51 27 51h20c.5 0 1-.4 1-1s-.4-.9-1-.9z"
+            />
+            <path
+              id="24-hours_svg__XMLID_2046_"
+              className="24-hours_svg__st0"
+              d="M12.3 16.3c-.5-.3-1.1-.1-1.4.3-2.3 3.5-3.3 7.7-2.8 11.8.5 4.2 2.4 8.1 5.5 11.1C17.3 43.2 22.1 45 27 45s9.7-1.8 13.4-5.4c7.4-7.2 7.4-19 0-26.2-3.1-3-7.1-4.9-11.4-5.3-4.2-.4-8.6.5-12.1 2.8-.5.3-.6.9-.3 1.3.3.5.9.6 1.4.3 6.7-4.1 15.5-3.1 21.1 2.3 6.6 6.5 6.6 17 0 23.4-3.3 3.2-7.7 4.8-12 4.8-4.4 0-8.7-1.6-12-4.8-5.7-5.5-6.6-14-2.4-20.5.2-.5.1-1.1-.4-1.4z"
+            />
+            <path
+              id="24-hours_svg__XMLID_2045_"
+              className="24-hours_svg__st0"
+              d="M16.5 22c-1.4 0-2.5 1.1-2.5 2.5 0 .5.4.9.9.9s.9-.4.9-.9c0-.4.3-.8.8-.8.7 0 .8.4.8.8 0 1.3-1.8 3.1-2.9 3.9-.3.2-.4.6-.3 1 .1.4.4.6.8.6h3.3c.5 0 .9-.4.9-.9s-.4-.9-.9-.9h-1c.9-1 1.9-2.4 1.9-3.7C19 23 18 22 16.5 22z"
+            />
+            <path
+              id="24-hours_svg__XMLID_2044_"
+              className="24-hours_svg__st0"
+              d="M20.9 22c-.5 0-.9.4-.9.9V26c0 .5.4.9.9.9h2.2v2.3c0 .5.4.9.9.9s.9-.4.9-.9v-6.3c0-.5-.4-.9-.9-.9s-.9.4-.9.9v2.3h-1.3v-2.3c0-.5-.4-.9-.9-.9z"
+            />
+            <path
+              id="24-hours_svg__XMLID_2043_"
+              className="24-hours_svg__st0"
+              d="M28.9 30c.4 0 .7-.2.9-.5l3.1-6.3c.2-.4 0-1-.5-1.2-.5-.2-1 0-1.3.4L28 28.7c-.2.4 0 1 .5 1.2.2.1.3.1.4.1z"
+            />
+            <path
+              id="24-hours_svg__XMLID_2042_"
+              className="24-hours_svg__st0"
+              d="M36.7 30h.3c.4 0 .8-.2.9-.6l2-6.3c.1-.3 0-.5-.1-.8-.2-.2-.5-.3-.8-.3h-4c-.5 0-1 .4-1 .9s.4.9 1 .9h2.8l-1.7 5.1c-.2.4.1.9.6 1.1z"
+            />
+            <path
+              id="24-hours_svg__XMLID_2040_"
+              className="24-hours_svg__st0"
+              d="M14 13c-.3 0-.5.1-.7.3-.2.2-.3.4-.3.7s.1.5.3.7.4.3.7.3c.3 0 .5-.1.7-.3s.3-.4.3-.7-.1-.5-.3-.7c-.2-.2-.4-.3-.7-.3z"
+            />
+            <path
+              id="24-hours_svg__XMLID_2039_"
+              className="24-hours_svg__st0"
+              d="M51 49c-.6 0-1 .4-1 1s.4 1 1 1c.5 0 1-.4 1-1s-.4-1-1-1z"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export default Svg24Hours;
